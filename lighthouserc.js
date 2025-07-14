42939860a125862e88098e652a5659e4b3ee54f4
CAU-ZIP <117087366+kauesp0007@users.noreply.github.com>
2025-07-14 07:09:25 -0300

    Create lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'npx http-server ./ -p 8080',
+      url: ['http://localhost:8080/index.html'],
+      numberOfRuns: 3
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['error', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }]
+      }
+    },
+    upload: {
+      target: 'temporary-public-storage'
+    }
+  }
+};
