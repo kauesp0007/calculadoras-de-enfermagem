988f9a618e06a4dc30e4827ceb74131edf669ef8
CAU-ZIP <kauesp07@hotmail.com>
2025-11-08 14:58:55 -0300

    Otimiza Tailwind CSS e remove CDN

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",         // Arquivos na pasta raiz
+    "./en/*.{html,js}",       // Inglês
+    "./es/*.{html,js}",       // Espanhol
+    "./fr/*.{html,js}",       // Francês
+    "./it/*.{html,js}",       // Italiano
+    "./de/*.{html,js}",       // Alemão
+    "./hi/*.{html,js}",       // Hindi
+    "./zh/*.{html,js}",       // Chinês (Mandarim)
+    "./ja/*.{html,js}",       // Japonês
+    "./ar/*.{html,js}",       // Árabe
+    "./ru/*.{html,js}",       // Russo
+    "./tr/*.{html,js}",       // Turco
+    "./ko/*.{html,js}",       // Coreano
+    "./nl/*.{html,js}",       // Holandês
+    "./pl/*.{html,js}",       // Polonês
+    "./sv/*.{html,js}",       // Sueco
+    "./id/*.{html,js}",       // Indonésio
+    "./vi/*.{html,js}",       // Vietnamita
+    "./uk/*.{html,js}"        // Ucraniano
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
