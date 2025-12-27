e79066f9d331a1a57ce91a3d7c037cdaa835851e
kauesp0007 <117087366+kauesp0007@users.noreply.github.com>
2025-12-27 19:51:46 +0000

    Atualiza sw.js (Automático)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20251227-193354-928';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20251227-195143-374';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
