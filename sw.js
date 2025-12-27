b24a9ecf32a3a316af81542b0239853614e206e3
CAU-ZIP <kauesp07@hotmail.com>
2025-12-27 18:35:32 -0300

    ALTERANDO DE TODOS OS IDIOMAS O CONSENT BANNER

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20251227-181625-663';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20251227-183510-141';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
