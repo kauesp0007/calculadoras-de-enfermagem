4756efdb491530421adc597ea904a658a7436788
CAU-ZIP <kauesp07@hotmail.com>
2025-12-27 21:42:23 -0300

    ATUALIZAÇÃO DAS TAG E ADSENSE

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20251227-205430-708';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20251227-214045-228';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
