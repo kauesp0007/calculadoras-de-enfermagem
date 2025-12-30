bd26c5c81f307e972297bfabd30e50ccd1036f99
CAU-ZIP <kauesp07@hotmail.com>
2025-12-30 08:27:49 -0300

    Revert "forum SEO"
    This reverts commit a9690e7fed10e4b309492f5a1d9ca90bf8423e98.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20251230-082450-883';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20251230-074338-968';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
