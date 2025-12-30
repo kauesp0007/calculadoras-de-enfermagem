d7b91b231e2e9d7621d237ea604ad592a4cc97aa
CAU-ZIP <kauesp07@hotmail.com>
2025-12-30 08:35:27 -0300

    LANG FORUM

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20251230-083001-808';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20251230-083448-689';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
