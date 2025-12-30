72ba3cba54c3aba4aa0a3c8068f7c4525de738d7
CAU-ZIP <kauesp07@hotmail.com>
2025-12-30 17:08:51 -0300

    ATUALIZANDO LANG SELECTOR DE TODOS OS IDIOMAS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20251230-170155-256';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20251230-170829-810';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
