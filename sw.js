80609c2b18066ef4720ce3e1c33de22ad8f702ac
CAU-ZIP <kauesp07@hotmail.com>
2026-01-07 21:00:59 -0300

    TRADUÇÃO SCRIOT CALC UK

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260107-201631-804';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260107-210033-759';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
