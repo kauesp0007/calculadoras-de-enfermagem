69bc6b3c33b065689222cebbcb17c0ce17f4289b
CAU-ZIP <kauesp07@hotmail.com>
2026-01-09 20:04:41 -0300

    Instrumentais cirurgicos arrumando as margens

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260109-175632-637';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260109-200345-694';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
