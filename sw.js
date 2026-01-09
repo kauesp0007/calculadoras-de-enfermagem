602fbfae26aec3588858f2f99a9571eb93305b85
CAU-ZIP <kauesp07@hotmail.com>
2026-01-09 20:29:05 -0300

    ESPAÇAMENTO ENTRE O TITULO E O MENU GLOBAL DAS PAGINAS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260109-200827-938';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260109-202834-037';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
