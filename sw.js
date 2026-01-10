81f99ab6a588bf8e6e6a0af18c58fc3fbe92a004
CAU-ZIP <kauesp07@hotmail.com>
2026-01-10 09:13:49 -0300

    ATUALIZANDO FORUM DE ENFERMAGEM COM IMAGENS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260110-061527-469';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260110-091331-661';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
