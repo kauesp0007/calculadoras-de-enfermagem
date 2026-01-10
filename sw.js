7f60d181bc36791efefeebffd1960003698dd03f
CAU-ZIP <kauesp07@hotmail.com>
2026-01-10 02:35:28 -0300

    ARRUMANDO O MENU GLOBAL CONTEUDO N ABRINDO

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260110-022036-222';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260110-023454-107';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
