9d2b8c601103c3de5ebff5791fa94761bc11747d
CAU-ZIP <kauesp07@hotmail.com>
2026-01-15 20:56:18 -0300

    PAGINA INSULINA ARRUMADA EM INGLES E ESPANHOL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260115-203144-523';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260115-205550-069';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
