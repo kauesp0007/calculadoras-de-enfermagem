ae38f8707b7c957a810b60370ce6bad8141ea2a2
CAU-ZIP <kauesp07@hotmail.com>
2026-01-16 05:56:26 -0300

    ARRUMANDO SBAR 3 PDF

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260116-054411-061';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260116-055604-677';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
