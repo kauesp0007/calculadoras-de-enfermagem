007e1441760ec846a8d164dee2eb6a236ade5515
CAU-ZIP <kauesp07@hotmail.com>
2026-01-18 07:34:02 -0300

    ITEM TEMPLATE + BIBLIOTECA MELHORADOS ESTILOS DOS BOTOES E OUTRAS MELHORIAS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260118-063534-703';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260118-073224-380';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
