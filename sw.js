6850fb748149cb39c7959a2369a8f34405255b85
CAU-ZIP <kauesp07@hotmail.com>
2026-01-20 18:20:41 -0300

    2 TENTATIVA DE TRADUÇÃO HAMILTON ESPANHOL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260120-180853-795';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260120-182000-042';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
