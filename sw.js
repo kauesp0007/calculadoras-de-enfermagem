8a56e1101da5f8d66f17c09b5b47e0e57b71d618
CAU-ZIP <kauesp07@hotmail.com>
2026-01-22 16:54:22 -0300

    ARUMANDO INDEX E GLOBAL SYLES CSS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260122-161042-378';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260122-165254-586';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
