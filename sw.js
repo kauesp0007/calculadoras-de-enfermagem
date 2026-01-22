8e1ae38e9dd22fc0301425ca67a9b914b0464865
CAU-ZIP <kauesp07@hotmail.com>
2026-01-22 05:50:17 -0300

    CORREÇÃO MAPA DO BRASIL LISTA DE NOTIFICAÇÃO COMPULSÓRIA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260122-053524-265';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260122-054935-005';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
