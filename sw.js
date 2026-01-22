809a4582bc130ad9f9b93632854d36883d903e41
CAU-ZIP <kauesp07@hotmail.com>
2026-01-22 02:30:34 -0300

    GLOBAL-SCRIPTS SEM ADSENSE PARA NAO DUPLICAR

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260121-235044-586';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260122-022512-452';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
