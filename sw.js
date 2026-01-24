4d2915a9e0fe6097f3137857b5102ccb31a102e5
CAU-ZIP <kauesp07@hotmail.com>
2026-01-24 01:35:51 -0300

    IMG LOG DOS ARQUIVOS IMG MANDADOS PARA A PASTA DE IMAGENS IMG/

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260123-192754-459';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260124-013524-549';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
