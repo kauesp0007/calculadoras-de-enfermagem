b8cc3c88a915578cf8bbd682cfbdf76e33ba9c28
CAU-ZIP <kauesp07@hotmail.com>
2026-02-05 22:44:50 -0300

    8ª ALTERAÇÃO ACLS MELHORIA DO TAMANHO DA IMAGEM

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260205-223148-973';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260205-224353-114';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
