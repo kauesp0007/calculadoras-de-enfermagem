5a17d405ba9cc2c9644c79ec2679b1476adec712
CAU-ZIP <kauesp07@hotmail.com>
2026-02-06 04:57:48 -0300

    correção do botao de idiomas do sbar

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260206-044707-363';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260206-045730-312';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
