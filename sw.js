048b1d9ca4352cafcce4923ec082a58a31452e30
CAU-ZIP <kauesp07@hotmail.com>
2026-02-07 01:02:52 -0300

    gasometria atualizada para os idiomas frances, indiano e italiano

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260207-004452-735';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260207-010203-455';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
