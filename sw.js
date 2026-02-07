5745907c27bc595dd653f2d4ac39c67a5c34a07e
CAU-ZIP <kauesp07@hotmail.com>
2026-02-07 02:56:02 -0300

    tradução de gasometria para ID JA RU

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260207-021216-114';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260207-025526-044';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
