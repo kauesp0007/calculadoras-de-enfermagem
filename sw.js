9b56aff686dbc3c011ed89c1c8cab0e50ae53e96
CAU-ZIP <kauesp07@hotmail.com>
2026-02-13 19:53:34 -0300

    correçao do adicional noturno

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260213-194601-299';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260213-195240-038';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
