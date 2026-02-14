9698418af5dc246226cd50d1102e93f094a5486f
CAU-ZIP <kauesp07@hotmail.com>
2026-02-14 20:43:02 -0300

    6ª tentativa de modernização da pagina dimensionamento de enfermagem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260214-203120-558';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260214-204142-317';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
