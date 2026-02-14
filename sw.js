c34af9ec35e81bf2c0b36de820594bda78067f7f
CAU-ZIP <kauesp07@hotmail.com>
2026-02-14 07:56:20 -0300

    MELHORIAS 2ª TENTATIVA DO NOVO BALANÇO HIDRICO

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260214-073502-319';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260214-075600-897';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
