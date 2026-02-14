0ad841ee72002ed8eea3558ea0a371c11c1848f8
CAU-ZIP <kauesp07@hotmail.com>
2026-02-14 06:51:39 -0300

    alteração de todos os globais styles css configurando a nova dimensao do footer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260214-053819-435';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260214-065101-257';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
