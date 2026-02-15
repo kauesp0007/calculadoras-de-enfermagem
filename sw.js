cdb7fec0ea2be690c8552054363a7df7c827d309
CAU-ZIP <kauesp07@hotmail.com>
2026-02-15 13:40:45 -0300

    TRADUÇÃO DE GASOMETRIA PARA TODOS OS IDIOMAS DO REPOSITORIO

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260215-121817-714';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260215-134008-209';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
