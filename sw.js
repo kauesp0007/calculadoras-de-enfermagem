a5022cca316c97815d8bd519217170ce3c67a9fa
CAU-ZIP <kauesp07@hotmail.com>
2026-02-15 14:23:13 -0300

    arrumando o caminho absoluto do global-scripts em todos os idiomas para corrigir o idioma que aparece

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260215-134526-580';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260215-141857-989';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
