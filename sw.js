c65d7b063e1267d7b2dfbba1a6bcf1c3b9abc30c
CAU-ZIP <kauesp07@hotmail.com>
2026-02-17 07:22:12 -0300

    melhoria dos preconecct do gosnell pt br

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-063349-988';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-072120-190';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
