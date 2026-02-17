e2c0835b18a1484c3c296f5960baadf2d2cb76ee
CAU-ZIP <kauesp07@hotmail.com>
2026-02-17 09:03:45 -0300

    modificação da ordem dos preconnects, preloader e prefetch de todos os ficheiros html do repositorio

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-073342-517';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-090133-620';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
@@ -1144,6 +1144,7 @@ const urlsToCache = [
   'favicon.ico',
   'filtro-index.js',
   'fix-cls.js',
+  'fix-head-tags.js',
   'flacc.html',
   'footer.html',
   'force-clear-capas.js',
