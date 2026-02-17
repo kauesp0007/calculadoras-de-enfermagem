1921c8f3966350bb84b5669ed0e714abdc3e2480
CAU-ZIP <kauesp07@hotmail.com>
2026-02-17 02:51:58 -0300

    Correção do index template do blog

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-023934-636';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-025112-320';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
