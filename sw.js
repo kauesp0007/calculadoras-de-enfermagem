d43a6da4e6d3055584fcc0ad11f7c6c52fca6e5e
CAU-ZIP <kauesp07@hotmail.com>
2026-02-17 00:43:15 -0300

    correção do caminho absoluto do menu global em portugues com /

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260215-214042-871';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260217-004251-218';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
