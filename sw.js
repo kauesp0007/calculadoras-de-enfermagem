562658fc6c3172aa05b3e9e1d2b81bf4047b9a2f
CAU-ZIP <kauesp07@hotmail.com>
2026-02-18 09:56:56 -0300

    TRADUÇÃO DE PERROCA PARA O ALEMAO E PARA O ÁRABE

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-092853-457';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-095506-359';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
