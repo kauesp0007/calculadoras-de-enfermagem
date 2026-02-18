e7d06e9f755db5e8a1db6ac349d6f08acfa67fde
CAU-ZIP <kauesp07@hotmail.com>
2026-02-18 06:31:40 -0300

    7ª TENTATIVA DE ARRUMAR O FICHEIRO PERROCA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-061909-850';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-063121-424';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
