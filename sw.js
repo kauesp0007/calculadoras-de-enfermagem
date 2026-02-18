a0eff8bd2477bfbf46f03cec9d1d1f47c17b2bbe
CAU-ZIP <kauesp07@hotmail.com>
2026-02-18 01:04:51 -0300

    correçao da barra de idiomas do blog

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-005612-927';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-010434-805';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
