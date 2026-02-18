59d70e485d067636c40cb95df45ed01627111130
CAU-ZIP <kauesp07@hotmail.com>
2026-02-18 07:26:03 -0300

    SOLUÇÃO FINAL PARA O NOVO FICHEIRO DE PERROCA PT BR

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-070039-824';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-072544-543';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
