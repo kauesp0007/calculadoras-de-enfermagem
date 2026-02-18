986890dec669e3dd9b0e2e1970d413a0c71d67f8
CAU-ZIP <kauesp07@hotmail.com>
2026-02-18 08:35:19 -0300

    tentativa de deixar o titulo da calculadora de perroca maior

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-082201-325';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260218-083457-523';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
