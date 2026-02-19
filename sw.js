b69dc37b76ca4d6c9fb31ac37201e70d79847da0
CAU-ZIP <kauesp07@hotmail.com>
2026-02-19 07:48:41 -0300

    voutando a origem de gotejamento

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260219-073518-741';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260219-074732-224';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
