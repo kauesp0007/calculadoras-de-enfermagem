7711c9f2b32919d6a72221aa189e176d3175e3d6
CAU-ZIP <kauesp07@hotmail.com>
2026-02-27 15:52:21 -0300

    atualização da pagina balanço hidrico em espanhol 1ª tentativa

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260227-154503-587';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260227-155042-373';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
