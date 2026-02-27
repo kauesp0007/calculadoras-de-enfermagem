1ac1df65c300e520a65df0a3e453dac0a9c70662
CAU-ZIP <kauesp07@hotmail.com>
2026-02-27 17:55:34 -0300

    3ª tentativa ESPANHOLgotejamento arrumando barra de idiomas

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260227-174211-428';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260227-175238-039';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
