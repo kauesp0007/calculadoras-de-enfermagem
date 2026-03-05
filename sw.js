21454d2ac8a95de21453a79e2e158b7a53e36c4c
CAU-ZIP <kauesp07@hotmail.com>
2026-03-05 18:03:48 -0300

    au teração da tag fontes clodflare com noscript em htmls que nao tinham

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260305-170756-585';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260305-180239-553';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
