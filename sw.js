e3bbb02012c9d469bbd1e6bd71de79dd4b3c219d
CAU-ZIP <kauesp07@hotmail.com>
2026-03-04 21:40:38 -0300

    3ª correção de balanço hidrico pt BR de espaçamento do card de h2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260304-212006-299';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260304-213948-235';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
