acd45308e63daf195fa6a9e2508f8d26f3c270ad
CAU-ZIP <kauesp07@hotmail.com>
2026-03-04 22:22:42 -0300

    reestruturação de balanco hidrico ESPANHOL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260304-221249-010';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260304-222201-752';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
