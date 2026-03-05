887f7e375d8a111461b98a27842273b3ad62dea3
CAU-ZIP <kauesp07@hotmail.com>
2026-03-04 23:09:21 -0300

    COLOCANDO O H2 DO FUGULIN DENTRO DE UM CARD AZUL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260304-224155-584';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260304-230851-665';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
