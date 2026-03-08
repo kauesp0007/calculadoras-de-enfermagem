909eabbe978234c34cd3725a56aa39cbb2a2e5d4
CAU-ZIP <kauesp07@hotmail.com>
2026-03-08 05:20:20 -0300

    tradução da REESTRUTURAÇÃO DE BRADEN EM INGLES

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-051123-642';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-051911-382';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
