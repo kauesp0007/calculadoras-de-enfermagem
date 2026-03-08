29f9d9ca24960d0b6eec17d231b7cbf192cfedc9
CAU-ZIP <kauesp07@hotmail.com>
2026-03-07 21:29:22 -0300

    braden ES faviicon.ico com /

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260307-210249-074';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260307-212900-792';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
