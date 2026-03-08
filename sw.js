e685f78446d1cafe6aca28891a4d14bacd4a43d1
CAU-ZIP <kauesp07@hotmail.com>
2026-03-08 05:23:57 -0300

    reestruturação braden ESPANHOL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-051911-382';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-052333-251';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
