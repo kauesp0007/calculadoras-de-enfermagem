41ee938c5560e2cd51f652480bfd471c85cd9b39
CAU-ZIP <kauesp07@hotmail.com>
2026-03-08 04:45:24 -0300

    TRADUÇÃO DE BRADEN EM ALEMAO REESTRUTURADA

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-042435-414';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-044450-733';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
