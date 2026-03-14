9641d08f949564c051ade743476c2d50f1b3094d
CAU-ZIP <kauesp07@hotmail.com>
2026-03-14 18:26:22 -0300

    Mudando o h2 de Perroca nas paginas PT EN DE ES IT E FR e adicionando paises em Fugulin PT BR

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260308-052333-251';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260314-182512-604';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
