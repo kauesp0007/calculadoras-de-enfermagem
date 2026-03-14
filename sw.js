1e626ce8e9511c163a9231f8599cda4c49059207
CAU-ZIP <kauesp07@hotmail.com>
2026-03-14 19:35:06 -0300

    arrumando o caminho absoluto de menu global de gotejamento ESPANHOL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260314-191249-452';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260314-193428-838';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
