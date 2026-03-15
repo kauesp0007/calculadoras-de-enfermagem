8c97ceeb111abb7b7b15ad6ee71372044d23058d
CAU-ZIP <kauesp07@hotmail.com>
2026-03-14 23:52:09 -0300

    melhoria do menu idiomas da biblioteca de enfermagem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260314-233934-662';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260314-235110-148';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
