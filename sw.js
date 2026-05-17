68888ec4ecd1374351553e65352f4cc5656603dc
CAU-ZIP <kauesp07@hotmail.com>
2026-05-17 15:39:05 -0300

    atualização biblioteca

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260315-004936-106';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260517-153805-633';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
