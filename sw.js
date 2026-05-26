609db6e6e1bcc750a252252dbde2ed108e29e517
CAU-ZIP <kauesp07@hotmail.com>
2026-05-26 20:29:49 -0300

    revisao saude mental 4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260526-195656-485';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260526-202931-041';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
