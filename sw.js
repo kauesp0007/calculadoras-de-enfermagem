f6a4383cbbc6fc3f119d22443d3e33b0f97b3e26
CAU-ZIP <kauesp07@hotmail.com>
2026-05-28 20:12:32 -0300

    COPSOQ VERSAO CURTA MENU GLOBAL

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260528-200711-564';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260528-201203-534';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
