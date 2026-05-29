89f630828655915245a5fc06a397176748709e05
CAU-ZIP <kauesp07@hotmail.com>
2026-05-29 01:48:54 -0300

    adição do anuncio multiflex via global-scripts.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-014306-405';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-014830-390';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
