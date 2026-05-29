99a6171642ec482280fbde5936b673eee6f7e26f
CAU-ZIP <kauesp07@hotmail.com>
2026-05-29 03:06:20 -0300

    correção exclusao do hrreflang das paginas que nao tem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-024323-565';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-030548-044';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
