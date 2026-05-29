844e263afc9cace1b5692f17b54a52c03f9bd022
CAU-ZIP <kauesp07@hotmail.com>
2026-05-29 04:38:17 -0300

    REGRAS PARA CRIAR UM BLOG

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-042750-055';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-043740-741';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
