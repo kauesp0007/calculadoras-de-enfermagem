6df5bda06fbe47dabbba0a4d39b0f7c90e98afc5
CAU-ZIP <kauesp07@hotmail.com>
2026-05-29 09:14:50 -0300

    melhorias na pagina biblioteca de enfermagem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-090419-913';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260529-091418-750';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
