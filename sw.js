4c8a2f795233de2c0692e7b14a981e9439de5219
CAU-ZIP <kauesp07@hotmail.com>
2026-06-05 15:47:52 -0300

    adição da barra de formatação no album de enfermagem

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260605-144619-847';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260605-154716-509';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
