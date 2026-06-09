92a4cea265c082d36eee41e1eb608fa7e9e800ca
CAU-ZIP <kauesp07@hotmail.com>
2026-06-09 19:17:25 -0300

    MELHORIA DA PAGINA EXAMES LABORATORIAIS

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260606-050124-546';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260609-191617-135';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
