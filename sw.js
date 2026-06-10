daf7cf1c2a7d6fbbc80aa72527253aa80407830a
CAU-ZIP <kauesp07@hotmail.com>
2026-06-10 02:36:49 -0300

    OTIMIZAÇÃO DE CARREGADOR DE FONTES -------TESTE--------

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = 'calculadoras-enfermagem-cache-20260610-021946-832';
+const CACHE_NAME = 'calculadoras-enfermagem-cache-20260610-023553-215';
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
@@ -2961,6 +2961,7 @@ const urlsToCache = [
   'nr1.html',
   'objetivo.html',
   'ofras.html',
+  'otimizar-fontes.js',
   'painad.html',
   'pelod.html',
   'pentagono.webp',
