0af812d10d4e561261dc2c7d4619a66987a99c08
CAU-ZIP <kauesp07@hotmail.com>
2026-06-14 05:46:37 -0300

    correção do global styles minimizado

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 Capas; // Define o nome e a versão do cache (AUTOMÁTICO via gerar-sw.js)
-const CACHE_NAME = "calculadoras-enfermagem-cache-20260614-045456-617";
+const CACHE_NAME = "calculadoras-enfermagem-cache-20260614-054305-204";
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
 const urlsToCache = [
