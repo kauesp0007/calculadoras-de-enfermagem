84842c643cc8d3aa0eaf15767c2e20c021b58801
CAU-ZIP <kauesp07@hotmail.com>
2026-06-27 16:02:13 -0300

    nova calculadora de medicamentos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260627-142754";
+const CACHE_VERSION = "20260627-160019";
 const CACHE_NAME = `calculadoras-enfermagem-cache-${CACHE_VERSION}`;
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
@@ -37,7 +37,6 @@ const urlsToCache = [
   'es/global-styles.css',
   'es/lang-selector.js',
   'favicon.ico',
-  'fix_mobile_render.js',
   'fonts/arabic/arabic-700.woff2',
   'fonts/arabic/arabic-regular.woff2',
   'fonts/chinese/chinese-700.woff2',
