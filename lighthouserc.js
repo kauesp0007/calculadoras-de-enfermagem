f347292caef13a82b745ab3b3738bbcbb144a240
CAU-ZIP <kauesp07@hotmail.com>
2026-07-06 21:48:21 -0300

    criação da biblioteca nanda INGLES E A PAGINA DIAGNOSTICOS NANDA INGLES

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,11 +2,11 @@ module.exports = {
   ci: {
     collect: {
       url: [
-        'http://localhost:8080/index.html',
-        'http://localhost:8080/braden.html',
-        'http://localhost:8080/fugulin.html',
-        'http://localhost:8080/dimensionamento.html',
-        'http://localhost:8080/gotejamento.html'
+        'http://localhost:8080/diagnosticosnanda.html',
+        'http://localhost:8080/zarit.html',
+        'http://localhost:8080/medicacoes.html',
+        'http://localhost:8080/instrumentais-cirurgicos.html',
+        'http://localhost:8080/gasometria.html'
       ],
       staticDistDir: './', // Onde o servidor local do Lighthouse deve rodar
       numberOfRuns: 3,     // Mantém 3 testes por página para uma média precisa
