fd31906a701f542a5df5af680c276ee35afe38c3
CAU-ZIP <kauesp07@hotmail.com>
2026-07-18 21:16:36 -0300

    Update tradutor_inteligente.py

--- automacoes/tradutor_inteligente.py
+++ automacoes/tradutor_inteligente.py
@@ -747,7 +747,7 @@ if __name__ == "__main__":
     # 🟢 ÁREA DE CONFIGURAÇÃO DIÁRIA (ALTERE APENAS AQUI) 🟢
     # =========================================================================
     
-    arquivos_originais = ["apache.html", "aldrete.html", "pews.html", "meows.html", "news.html"]
+    arquivos_originais = ["apgar.html", "glasgow.html", "asa.html"]
     idiomas_alvo = ["en", "es", "de", "it", "fr", "hi", "zh", "ar", "ja", "ru", "ko", "tr", "nl", "pl", "sv", "id", "vi", "uk"] 
     
     # =========================================================================
