41c2bb9dc03bc0c72f3b257a9da70461edc4f63f
CAU-ZIP <kauesp07@hotmail.com>
2026-09-03 23:24:41 -0300

    correção do botao forum do index das 18 paginas iniciais

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260903-230402";
+const CACHE_VERSION = "20260903-232246";
 const CACHE_NAME = `calculadoras-enfermagem-cache-${CACHE_VERSION}`;
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
@@ -630,6 +630,7 @@ const urlsToCache = [
   'relatorios/cwv-gate/2026-09-04T01-33-00.json',
   'relatorios/cwv-gate/2026-09-04T01-35-14.json',
   'relatorios/cwv-gate/2026-09-04T01-51-57.json',
+  'relatorios/cwv-gate/2026-09-04T02-14-44.json',
   'relatorios/impacto/2026-09-02T10-01-10.json',
   'relatorios/impacto/2026-09-02T10-55-46.json',
   'relatorios/impacto/2026-09-02T10-59-28.json',
@@ -743,6 +744,7 @@ const urlsToCache = [
   'relatorios/impacto/2026-09-04T01-33-02.json',
   'relatorios/impacto/2026-09-04T01-35-15.json',
   'relatorios/impacto/2026-09-04T01-51-58.json',
+  'relatorios/impacto/2026-09-04T02-14-45.json',
   'relatorios/multiplex-reserva.json',
   'relatorios/orquestracao/task-0085d6c28f25.json',
   'relatorios/orquestracao/task-2dfc9d3f72a9.json',
