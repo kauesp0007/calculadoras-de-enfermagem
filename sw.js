3645b538a0043cabe50643175bb3737eb985d947
CAU-ZIP <kauesp07@hotmail.com>
2026-09-11 08:08:31 -0300

    tentando implementar o squezzy

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "20260911-060958";
+const CACHE_VERSION = "20260911-075102";
 const CACHE_NAME = `calculadoras-enfermagem-cache-${CACHE_VERSION}`;
 
 // O SCRIPT DE BUILD VAI INJETAR A LISTA DE ARQUIVOS AQUI
@@ -676,6 +676,9 @@ const urlsToCache = [
   'relatorios/cwv-gate/2026-09-11T08-39-35.json',
   'relatorios/cwv-gate/2026-09-11T08-41-10.json',
   'relatorios/cwv-gate/2026-09-11T09-07-20.json',
+  'relatorios/cwv-gate/2026-09-11T10-08-35.json',
+  'relatorios/cwv-gate/2026-09-11T10-10-47.json',
+  'relatorios/cwv-gate/2026-09-11T10-21-54.json',
   'relatorios/impacto/2026-09-02T10-01-10.json',
   'relatorios/impacto/2026-09-02T10-55-46.json',
   'relatorios/impacto/2026-09-02T10-59-28.json',
@@ -834,6 +837,9 @@ const urlsToCache = [
   'relatorios/impacto/2026-09-11T08-39-35.json',
   'relatorios/impacto/2026-09-11T08-41-11.json',
   'relatorios/impacto/2026-09-11T09-07-21.json',
+  'relatorios/impacto/2026-09-11T10-08-36.json',
+  'relatorios/impacto/2026-09-11T10-10-48.json',
+  'relatorios/impacto/2026-09-11T10-21-55.json',
   'relatorios/multiplex-reserva.json',
   'relatorios/orquestracao/task-0085d6c28f25.json',
   'relatorios/orquestracao/task-2dfc9d3f72a9.json',
